d0c6d7c5f137707b8f33bffef2cafd3201afc3b5
Meghna Patel <meghna10jan21@yahoo.com>
2022-11-09 13:48:26 -0600

    Activity-iM-5

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,20 +10,32 @@ module.exports = (sequelize, DataTypes) => {
     }
 
   };
-  User.init({
-    userId: {
-      type: DataTypes.SMALLINT,
-      primaryKey: true,
-      autoIncrement: true
 
-    },
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
+    
+User.init({
+  userId: {
+    type: DataTypes.SMALLINT,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstName: DataTypes.STRING,
+  lastName: DataTypes.STRING,
+  email: DataTypes.STRING,
+  role: {
+      type: DataTypes.ENUM,
+      values: [
+        'reviewer',
+        'admin',
+      ],
+  },
+  passwordDigest: DataTypes.STRING
+},
+{
+
+
     sequelize,
     underscored: true,
     modelName: 'User',
   });
-  return User;
+return User;
 };
